8a1e5ffcc9df9776af06780b95a1c639a1d5548e
Jean Paulo Silva <jeanps95@gmail.com>
2019-12-11 17:04:41 -0300

    add new script joy_move.py

---
+++ build/joy_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jean/joy_ws/src/joy_test
+# Build directory: /home/jean/joy_ws/build/joy_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
