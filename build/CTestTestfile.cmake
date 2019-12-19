7dd87aceb00c296fa4b378ef6905d78edc5a4033
Jean Paulo Silva <jeanps95@gmail.com>
2019-12-19 14:14:05 -0300

    removed build and devel

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jean/joy_ws/src
-# Build directory: /home/jean/joy_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("joy_test")
